9118a286ab376a62cd1626d1a44c607c45a5cec2
Group3-NotAKeylogger <96501948+Group3-NotAKeylogger@users.noreply.github.com>
2021-12-22 15:49:24 +0200

    Add files via upload

---
+++ Install-Packages.bat
@@ -0,0 +1,12 @@
+echo Ahmed Basem, Ahmed Nader , Nour Sharaky
+echo This is a simple batch script to install Required Libraries
+
+pip install tk
+pip install zipfile36
+pip install cryptography
+pip install pynput
+pip install pywin32
+pip install pyscreenshot
+pip install pillow
+
+set /p DUMMY=Hit ENTER to continue...
